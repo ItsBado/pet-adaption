15d97932a017e44a2f1c52d1b88287458c5c9aa0
ItsBado <badodado1@gmail.com>
2024-12-25 15:26:38 +0300

    Nearly finish pet card feature

--- main.js
+++ main.js
@@ -23,6 +23,10 @@ async function petsArea() {
     const clone = template.content.cloneNode(true)
 
     clone.querySelector("h3").textContent = pet.name
+    clone.querySelector(".pet-info").textContent = pet.description
+    clone.querySelector(".pet-age").textContent = createAgeText(pet.birthYear)
+    clone.querySelector(".pet-card-photo img").src = pet.photo
+    clone.querySelector(".pet-card-photo img").alt = `A ${pet.species} named ${pet.name}`
 
     wrapper.appendChild(clone)
   })
@@ -31,3 +35,13 @@ async function petsArea() {
 }
 
 petsArea()
+
+function createAgeText(birthYear) {
+  const currentYear = new Date().getFullYear()
+  const age = currentYear - birthYear
+
+  if (age == 1) return "1 year old"
+  if (age == 0) return "Less than a year old"
+
+  return `${age} years old`
+}
